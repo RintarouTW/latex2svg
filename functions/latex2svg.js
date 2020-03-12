73901fb3b7095f4f91f009c54a779070d91292b5
周樹禾 <shuho.chou@gmail.com>
2020-03-13 05:40:40 +0800

    Don't support GET request

--- functions/latex2svg.js
+++ functions/latex2svg.js
@@ -21,13 +21,20 @@ function latex2svg(latex) {
 
 exports.handler = function(event, context, callback) {
 
-  data = event.body ? event.body : '{ "latex" : "\$\$\\LaTeX\\frac{1}{3}\$\$"}';
   json = {};
 
-  try {
-    json = JSON.parse(data);
-  } catch (err) { 
-    console.log ("Failed to parse as JSON:", data); 
+  if (event.httpMethod == "GET") {
+
+    json = { "latex" : "\$\$\\LaTeX\\frac{1}{3}\$\$"};
+
+  }  else {
+
+    data = event.body;
+    try {
+      json = JSON.parse(data);
+    } catch (err) { 
+      console.log ("Failed to parse as JSON:", data); 
+    }
   }
 
   console.log(`json.latex =`, json.latex);
